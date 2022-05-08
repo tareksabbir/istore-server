1f93dc881444a4347bcf0171774751aefc62df84
MD.Tarek Rahman <tareksabbir4599@gmail.com>
2022-05-08 17:34:54 +0600

    mongodb added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
+
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,11 +13,28 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.th5av.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    console.log('db is connected');
-    client.close();
-});
+
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db('iStore').collection('product');
+
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products)
+        })
+
+
+
+    }
+    finally {
+
+    }
+
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
